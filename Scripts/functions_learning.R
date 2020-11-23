be160e9879ae82c337aeae05cb1c1975d781a83a
Apex619 <z3375125@ad.unsw.edu.au>
2020-11-23 14:46:52 +1100

    Updates

---
+++ Scripts/functions_learning.R
@@ -0,0 +1,158 @@
+#install.packages("pacman") # run this line if you don't have pacman installed
+# loading packages
+pacman::p_load(readxl, tidyr, dplyr, magrittr, lubridate, stringr)
+
+#here is the excel sheet i enter data into, to give you an idea of what data i want from the files produced by the automated experimental (Zantiks) units
+
+#Aversive Pilots.xlsx
+
+#want <- read_excel("Aversive Pilots.xlsx", sheet = 1)
+
+#and here are some examples of the csv files that the units produce
+
+#trial-20190221T041305.csv 
+#trial-20190221T041317.csv 
+
+#have <- read.csv("trial-20190221T041305.csv", header = F) 
+
+#i want to add up all the baseline times and distances & and the probe times and distances, but instead of zone specific, colour specific. so instead of collating the data straight down the columns. for zone distances and zone timers, the colours alternate so i need to add them up diagonally down in a zig zag direction the outcome should be zone timers and zone distances for each arena (which is each fish) for both the baseline and the probe phase please let me know if this doesn’t make any sense thanks! also, each .csv file is from one of the 4 Zantiks Units after an experiment. so i have to extract data from 4 of these files per experiment.
+
+# Function for importing csv and exporting as long format
+
+# testing function
+#filepath = "trial-20190221T041317.csv"
+#filepath = "./raw data/trial-20190221T041317.csv"
+import_csv <- function(filepath){
+  
+  have <- read.csv(filepath, header = F)
+
+# extracting fish_ID for each arena, the date, and time
+fish <- strsplit(as.character(have[2,4]), " ") %>% unlist
+date <- as.Date(have[1,3])
+time <- substr(have[1,3], 12, 20)
+
+# keeping only rows that have data
+data <- have[have$V12 != "", ]
+
+# extracting the four arenas
+arena <- list()
+pos <- c(8, 10, 12, 14) # position of arena columns
+for(i in 1:4){
+  arena[[i]] <- data[, c(1, 5:7, pos[i]:(pos[i]+1))]
+  names(arena[[i]]) <- paste0("C", 1:ncol(arena[[i]]))
+  names(arena)[[i]] <- fish[i]
+}
+
+# writing function for dealing with one arena
+#a = arena[[1]]
+arena.func <- function(a){
+  # filling in blank rows
+  blank <- (1:length(a$C2))[a$C2 == ""]
+  chunks <- seq(1, length(blank), by = 3)
+  for(i in 1:length(chunks)){
+    a[blank[chunks[i]]:(blank[chunks[i]]+2),2:3] <- a[(blank[chunks[i]]-1),2:3]
+  }
+  # putting data in long form
+  a <- rbind(a[,-6], a[,-5] %>% rename(C5 = C6))
+  
+  # making a column for the arena and type of test 
+  arena_test <- a$C5
+  # finding position of numbers
+  char.num <- as.numeric(arena_test %>% as.character)
+  pos.char <- which(is.na(char.num)==T)
+  for(i in 1:length(pos.char)){
+    arena_test[(pos.char[i]+1):(pos.char[i]+3)] <- arena_test[pos.char[i]]
+  }
+  a$arena_test <- arena_test
+  
+  # removing unnecessary rows
+  a <- a[!is.na(char.num),] %>% rename(time = C1,
+                                       type = C2,
+                                       unit = C3,
+                                       variable = C4,
+                                       number = C5)
+  
+  # making column for arena, zone, and check/grey
+  splt <- strsplit(as.character(a$arena), "_")
+  a$arena <- lapply(splt, function(x) x[1]) %>% unlist 
+  a$zone <- lapply(splt, function(x) x[2]) %>% unlist 
+  a$test <- lapply(splt, function(x) x[3]) %>% unlist
+  
+  a <- dplyr::select(a, -arena_test)
+  
+  return(a)
+  
+}
+
+# applying function to all arenas
+df <- lapply(arena, function(x) arena.func(x)) %>% bind_rows()
+
+# adding columns for fish and date and the file number
+df %<>% mutate(marking = case_when(arena == "ARENA1" ~ fish[1],
+                                  arena == "ARENA2" ~ fish[2],
+                                  arena == "ARENA3" ~ fish[3],
+                                  arena == "ARENA4" ~ fish[4]),
+               date = date,
+               filepath = filepath)
+
+# same as this
+# df <- df %>% mutate(marking = case_when(arena == "ARENA1" ~ fish[1],
+#                                    arena == "ARENA2" ~ fish[2],
+#                                    arena == "ARENA3" ~ fish[3]),
+#                date = date,
+#                filepath = filepath)
+
+# removing "total" columns
+df <- df[!str_detect(as.character(df$unit), "TOTAL"),]
+
+# fixing numeric column
+df$number <- as.numeric(df$number)
+
+return(df)
+}
+
+#function for adding day column
+add_day <- function(x) {
+  x %>% mutate(day = case_when (grepl("2019-10-08", date) ~ "1",
+                                grepl("2019-10-09", date) ~ "2",
+                                grepl("2019-10-10", date) ~ "3",
+                                grepl("2019-10-11", date) ~ "4",
+                                grepl("2019-10-14", date) ~ "5",
+                                grepl("2019-10-15", date) ~ "6",
+                                grepl("2019-10-16", date) ~ "7",
+                                grepl("2019-10-17", date) ~ "8"))
+}
+
+add_exp <- function(x) {
+  x %>% mutate(exp = case_when (grepl("BLUGRE", filepath) ~ "BLUGRE"))
+}
+
+add_set <- function(x) {
+  x %>% mutate(set = case_when (grepl("c1", filepath) ~ "control",
+                                grepl("c2", filepath) ~ "control",
+                                grepl("c3", filepath) ~ "control",
+                                grepl("c4", filepath) ~ "control",
+                                grepl("t1", filepath) ~ "treatment",
+                                grepl("t2", filepath) ~ "treatment",
+                                grepl("t3", filepath) ~ "treatment",
+                                grepl("t4", filepath) ~ "treatment"))
+}
+
+add_zone <- function(x) {
+  x %>%   mutate(zone = case_when (grepl("CS", test) ~ "CS",
+                                   grepl("NON", test) ~ "NON"))
+}
+
+add_tod <- function(x) {
+  x %>% mutate(tod = case_when (grepl("AM", filepath) ~ "AM",
+                                grepl("NOON", filepath) ~ "NOON",
+                                grepl("PM", filepath) ~ "PM")) 
+}
+
+#function that spreads data and adds difference
+add_diff <- function(x) {
+  
+  x[,7] = NULL
+  
+  x %>% spread(type, time) %>% mutate(difference = (BASELINE - PROBE))
+}
---
+++ Scripts/functions_repeatability.R
@@ -0,0 +1,172 @@
+#### Custom functions
+
+#Calculating repeatability for learning (Zantiks)
+rpt_learning <- function(df) {
+  x <- rpt(difference ~ (1 |  fishID), grname = "fishID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances for learning
+rpt_within_learning <- function(df) {
+  rpt(scale(difference) ~ (1 |  fishID), grname = c("fishID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+
+#Calculating repeatability for mean speed
+rpt_speed <- function(df) {
+  x <- rpt(mean_speed ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances for mean speed
+rpt_within_speed <- function(df) {
+  rpt(scale(mean_speed) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for zone_05_duration
+rpt_zone05 <- function(df) {
+  x <- rpt(zone_05_dur ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances
+rpt_within_zone05 <- function(df) {
+  rpt(scale(zone_05_dur) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+
+
+#Calculating repeatability for zone_05_duration (Optimism)
+rpt_optimism_zone05 <- function(df) {
+  x <- rpt(zone_05_dur ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+
+#Calculating repeatability for body length (unadjusted for week)
+rpt_length <- function(df) {
+  x <- rpt(Fish_Length_cm ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Calculating repeatability for body length (adjusted for week)
+rpt_length2 <- function(df) {
+  x <- rpt(Fish_Length_cm ~ Week + (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (body length, adjusted for week)
+rpt_within_length <- function(df) {
+  rpt(scale(Fish_Length_cm) ~ Week + (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for body weight (unadjusted for week)
+rpt_weight <- function(df) {
+  x <- rpt(Weight.g. ~ Week + (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Calculating repeatability for body weight with week (adjusted for week)
+rpt_weight2 <- function(df) {
+  x <- rpt(Weight.g. ~ Week + (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (body weight without week)
+rpt_within_weight <- function(df) {
+  rpt(scale(Weight.g.) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Obtaining within and between-individual variances (body weight with week)
+rpt_within_weight2 <- function(df) {
+  rpt(scale(Weight.g.) ~ Week + (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+
+#Obtaining within and between-individual variances (total distance)
+rpt_within_tot_dist <- function(df) {
+  rpt(scale(tot_dist) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 100, npermut = 100, ratio = FALSE)
+}
+
+#Calculating repeatability for total distance travelled
+rpt_tot_dist <- function(df) {
+  x <- rpt(tot_dist ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 100, npermut = 100)
+}
+
+#Obtaining within and between-individual variances (time spent in low zone)
+rpt_within_between_low_dur <- function(df) {
+  rpt(scale(low_dur) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for time spent in low zone
+rpt_low_dur <- function(df) {
+  x <- rpt(low_dur ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (time spent in mid zone)
+rpt_within_between_mid_dur <- function(df) {
+  rpt(scale(mid_dur) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for time spent in mid zone
+rpt_mid_dur <- function(df) {
+  x <- rpt(mid_dur ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (time spent in high zone)
+rpt_within_between_high_dur <- function(df) {
+  rpt(scale(sqrt(high_dur)) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for time spent in high zone
+rpt_high_dur <- function(df) {
+  x <- rpt(sqrt(high_dur) ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (time spent in high zone) F0 ONLY
+rpt_within_between_high_dur2 <- function(df) {
+  rpt(scale(log(high_dur+1)) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for time spent in high zone F0 ONLY
+rpt_high_dur2 <- function(df) {
+  x <- rpt(log(high_dur+1) ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (time spent freezing)
+rpt_within_between_freezing_dur <- function(df) {
+  rpt(scale(log(freezing_dur+1)) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for time spent freezing 
+rpt_freezing_dur <- function(df) {
+  x <- rpt(log(freezing_dur+1) ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (latency to high zone)
+rpt_within_between_latency <- function(df) {
+  rpt(scale(latency_high) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for latency to the high zone
+rpt_latency <- function(df) {
+  x <- rpt(latency_high ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+#Obtaining within and between-individual variances (entries to high zone)
+rpt_within_between_freq_high <- function(df) {
+  rpt(scale(sqrt(freq_high)) ~ (1 |  Fish_ID), grname = c("Fish_ID", "Residual"), data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000, ratio = FALSE)
+}
+
+#Calculating repeatability for entries to the high zone
+rpt_freq <- function(df) {
+  x <- rpt(sqrt(freq_high) ~ (1 |  Fish_ID), grname = "Fish_ID", data = df, datatype = "Gaussian", nboot = 10000, npermut = 10000)
+}
+
+
+
+# Writing functions for getting the difference between two tank's repeatablities
+unlist_rptr <- function(rpt_model) { #unlisting the bootstrapped distribution 
+  unlist(rpt_model$R_boot)
+}
+
+difference_boot <- function(boot1,boot2) { #calculating difference between tall and short (depending on which has the higher repeatability)
+  boot1 - boot2
+}
+
+quantiles_diff_boot <- function(from_diff_boot) { #obtaining quantiles at 2.5% and 97.5% (becomes 95% CI)
+  quantile(from_diff_boot, c(0.025, 0.975))
+} 
+
