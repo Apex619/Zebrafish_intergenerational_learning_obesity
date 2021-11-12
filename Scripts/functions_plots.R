1ad2147ee2c21ea410e757c2e3c110f9ed0c1c28
Hamza Anwer <38271660+Apex619@users.noreply.github.com>
2021-11-12 15:08:02 +1100

    Edits to plots

--- Scripts/functions_plots.R
+++ Scripts/functions_plots.R
@@ -4,7 +4,7 @@
 violin_plot_custom_hamza <- function(df,param,lab1,lab2) { #only need to add dataframe, parameter, and labels for axes
   p <- ggplot(data = df,aes(x = Tank, y = param, fill = Tank))+
     # scale_fill_manual(values=c("#1F968BFF","#74D055FF", "39568CFF","#440154FF"))+ 
-    scale_fill_manual(values=c("#E69F00","#56B4E9", "#009E73","#999999"))+#color scheme
+    scale_fill_manual(values=c("#009E73","#E69F00","#56B4E9","#999999"))+#color scheme
     geom_violin(alpha=0.4, position = position_dodge(width = .75),size=0.5,color="black")+ #violin plot to display distribution density
     geom_boxplot(notch = TRUE,  outlier.size = -1, color="black",lwd=0.9, alpha = 0.5)+#boxplot to display quantiles
     geom_point( shape = 21,size=2, position = position_jitterdodge(), color="black",alpha=1)+#scatterplot of individual data points
@@ -41,7 +41,7 @@ violin_plot_custom_hamza2 <- function(df,param,lab1,lab2) { #only need to add da
     font("legend.text",size = 15)+
     theme(legend.position = "none")+
   theme(axis.title.x=element_blank())+
-    scale_fill_manual(values = c("#E69F00","#56B4E9", "#009E73","#999999"), name = "Tank", 
-                      labels = (c("F1M", "F1P", "F1C", "F1T")))
+    scale_fill_manual(values = c("#009E73","#E69F00","#56B4E9","#999999"), name = "Tank", 
+                      labels = (c("F1C", "F1M", "F1P", "F1T")))
   
 }
